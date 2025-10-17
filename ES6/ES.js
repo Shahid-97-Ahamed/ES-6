ef0fd440a6d50b58a54ff29632a2157d6050a155
Shahid Ahamed <shahid.07ahamed@gmail.com>
2025-10-17 21:38:46 +0900

    update

--- ES6/ES.js
+++ ES6/ES.js
@@ -14,11 +14,55 @@
 
 // console.log(sum(10,20));
 
-document.getElementById("test").addEventListener('click',
+// document.getElementById("test").addEventListener('click',
    
    
-    ()=>{
-    console.log('Hello!!! I am ES6');
-}
+//     ()=>{
+//     console.log('Hello!!! I am ES6');
+// }
 
-)
+// )
+
+
+// sapared Array
+
+// example-1
+
+const name=['Shahid','Ahamed','Rabbi'];
+
+const newArray=["name:",...name];
+
+console.log(...newArray);
+
+// example-2
+const number=[10,20,30,40,50];
+
+console.log(Math.max(number));
+console.log(Math.max(...number));
+
+// example-3
+const mixedArray=[1,2,3,"four",5];
+
+console.log(...mixedArray); 
+// output: 1 2 3 four 5
+
+// example-4
+const arr1=[1,2,3];
+const arr2=[4,5,6];
+const combinedArray=[...arr1,...arr2];
+console.log(...combinedArray);
+
+// output: 1 2 3 4 5 6
+
+// example-5
+const example5Array=[1,2,3,4,5,6];
+console.log(...example5Array);
+
+// output : 1 2 3 4 5 6
+
+// example-6
+function sum(x,y,z){
+    return x+y+z;
+}
+const numbers=[1,2,3];
+console.log(sum(...numbers));
