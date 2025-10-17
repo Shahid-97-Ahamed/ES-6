18af3ef6bbf0bb5593e7df1ed95acb3e7a1b49bd
Shahid Ahamed <shahid.07ahamed@gmail.com>
2025-10-17 21:19:51 +0900

    update

--- ES6/ES.js
+++ ES6/ES.js
@@ -1,15 +1,24 @@
-// step-1(empty parenthasis)
+// // step-1(empty parenthasis)
 
-const test=()=>console.log('Shahid');
-test()
+// const test=()=>console.log('Shahid');
+// test()
 
-// step-2(no paranthasis)
+// // step-2(no paranthasis)
 
-const squear=a=>a*a;
-console.log(squear(10));
+// const squear=a=>a*a;
+// console.log(squear(10));
 
-// step-2(paranthasis)
+// // step-2(paranthasis)
 
-const sum=(a,b)=>a+b;
+// const sum=(a,b)=>a+b;
 
-console.log(sum(10,20));
+// console.log(sum(10,20));
+
+document.getElementById("test").addEventListener('click',
+   
+   
+    ()=>{
+    console.log('Hello!!! I am ES6');
+}
+
+)
