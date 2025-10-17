5d0e4af341f263c1bc928affe299198264e2196f
Shahid Ahamed <shahid.07ahamed@gmail.com>
2025-10-17 21:01:32 +0900

    first commit

---
+++ ES6/ES.js
@@ -0,0 +1,15 @@
+// step-1(empty parenthasis)
+
+const test=()=>console.log('Shahid');
+test()
+
+// step-2(no paranthasis)
+
+const squear=a=>a*a;
+console.log(squear(10));
+
+// step-2(paranthasis)
+
+const sum=(a,b)=>a+b;
+
+console.log(sum(10,20));
