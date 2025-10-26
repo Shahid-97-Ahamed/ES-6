c72fd77e30ffcd4f1bb4ac59e3a92cb80f2a4ba1
Shahid Ahamed <shahid.07ahamed@gmail.com>
2025-10-27 03:03:05 +0900

    create div with jeson

--- ES6/jeson.js
+++ ES6/jeson.js
@@ -47,3 +47,84 @@
     //     console.log(users[2]);
 
     // }
+
+    // const moreData = () => {
+    //     fetch("https://jsonplaceholder.typicode.com/users")
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         showUser(data);
+    //     })
+    // }       
+
+    // const showUser = users => {
+    //     //part-1
+    //     // console.log(users);
+
+    //     // part-2
+    //     // for(let i=0; i<users.length; i++){
+    //     //     const user = users[i];
+    //     //     console.log(user);
+    //     // }
+
+    //     for(const user of users){
+    //         console.log(user.name);
+    //         const li = document.createElement("li");
+    //         li.innerText = user.name;
+    //         document.getElementById("user-container").appendChild(li);
+    //     }
+    // }
+
+    //create a div and display user info
+
+    // const displayPosts = () => {
+    //     fetch("https://jsonplaceholder.typicode.com/posts")
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         showPosts(data);
+    //     })
+    // }
+
+    // const showPosts = (posts) => {
+        
+    //     const postContainer = document.getElementById("cardContainer");
+    //     for (const post of posts) {
+    //         console.log(post);
+    //         const div = document.createElement("div");
+    //         div.classList.add("card");
+    //         div.innerHTML = `
+    //         <h1>${post.title}</h1>
+    //         <p>${post.body}</p>
+    //         <button>Read More</button>
+    //         `;
+    //         postContainer.appendChild(div);
+    //     }
+    // }
+    // displayPosts();
+
+
+    const displayPosts = () => {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(res => res.json())
+        .then(data => {
+            showPosts(data);
+        })  
+    }
+
+    const showPosts = (posts) => {
+        const postContainer = document.getElementById("cardContainer");
+
+        for (const post of posts) {
+            console.log(post);
+            const div = document.createElement("div");
+            div.classList.add("card");
+            div.innerHTML =`
+            <h2>${post.title}</h2>
+            <p>${post.body}</p>
+            <button>Read More</button>
+            `;
+            postContainer.appendChild(div);
+          
+        }
+    }
+
+    displayPosts();
