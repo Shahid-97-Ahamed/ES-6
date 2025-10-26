e95a7987f67d15db28db2737878695294ffb4e54
Shahid Ahamed <shahid.07ahamed@gmail.com>
2025-10-27 02:05:03 +0900

    jason update

--- ES6/jeson.js
+++ ES6/jeson.js
@@ -1,21 +1,49 @@
 // JavaScript Object Notation (JSON)
 
-const person ={
-    name:"Shahid Ahamed",
-    age:24,
-    profession:"Web Developer"
-}
+// const person ={
+//     name:"Shahid Ahamed",
+//     age:24,
+//     profession:"Web Developer"
+// }
 
-//Object to JSON
-const newPerson = JSON.stringify(person);
-console.log(newPerson);
-// console.log(typeof newPerson);
-// console.log(typeof person);
+// //Object to JSON
+// const newPerson = JSON.stringify(person);
+// console.log(newPerson);
+// // console.log(typeof newPerson);
+// // console.log(typeof person);
 
-//JSON to Object
+// //JSON to Object
 
-const jsonPerson = '{"name":"Shahid Ahamed","age":24,"profession":"Web Developer"}';
-const objPerson = JSON.parse(jsonPerson);
-console.log(objPerson);
-// console.log(typeof jsonPerson);
-// console.log(typeof objPerson);   
+// const jsonPerson = '{"name":"Shahid Ahamed","age":24,"profession":"Web Developer"}';
+// const objPerson = JSON.parse(jsonPerson);
+// console.log(objPerson);
+// // console.log(typeof jsonPerson);
+// // console.log(typeof objPerson);   
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+
+    //   const loadData = () =>{
+    //     fetch('https://jsonplaceholder.typicode.com/todos/1')
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         console.log(data)
+    //     })
+    //   }
+
+
+    // const loadUserData = () => {
+    //     fetch("https://jsonplaceholder.typicode.com/users")
+
+    // .then(res => res.json())
+    // .then(data => {
+    //     console.log(data);
+    //     displayUser(data);
+    // })
+    // }
+
+    // const displayUser = users => {
+    //     console.log(users[2]);
+
+    // }
